0457bd34d74b963795c3f0eb9c49a633c7512bf5
Axely7 <axely7@hotmail.com>
2023-12-12 13:47:00 -0600

    prisma and trpc done

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,16 +2,24 @@ import React from "react";
 // @ts-ignore-next-line
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
+import { db } from "../../db/index";
+import { Dashboard } from "@/components/Dashboard";
 
-const Dashboard = async () => {
+const Page = async () => {
   const { getUser } = await getKindeServerSession();
   const user = await getUser();
 
   if (!user || !user.id) redirect("/auth-callback?origin=dashboard");
 
-  
+  const dbUser = await db.user.findFirst({
+    where: {
+      id: user.id,
+    },
+  });
 
-  return <div>{user?.email}</div>;
+  if (!dbUser) redirect("/auth-callback?origin=dashboard");
+
+  return <Dashboard />;
 };
 
-export default Dashboard;
+export default Page;
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -2,21 +2,32 @@ import { router, publicProcedure } from './trpc';
 // @ts-ignore-next-line
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { TRPCError } from '@trpc/server';
+import { db } from '@/db';
 
- 
 export const appRouter = router({
-  authCallback: publicProcedure.query(() => {
-    const {getUser} =  getKindeServerSession()
-    const user = getUser()
-
+  authCallback: publicProcedure.query(async() => {
+    const {getUser} = await  getKindeServerSession()
+    const user = await getUser()
     if(!user.id || !user.email){
       throw new TRPCError({code: "UNAUTHORIZED"})
+    }
+
+    const dbUser = await db.user.findFirst({where: {id: user.id}})
+
+    if(!dbUser){
+      // create user in db
+      await db.user.create({
+        data: {
+          id: user.id,
+          email: user.email
+        }
+      })
     }
 
     return {success: true}
   })
 });
- 
+
 // Export type router type signature,
 // NOT the router itself.
 export type AppRouter = typeof appRouter;
