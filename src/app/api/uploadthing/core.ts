a179f87bf18c4f75d85818068a6984373f9b3b48
Axely7 <axely7@hotmail.com>
2024-01-11 13:05:10 -0600

    implementing openai api

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -2,6 +2,10 @@ import { createUploadthing, type FileRouter } from "uploadthing/next";
 // @ts-ignore-next-line
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { db } from '../../../db/index';
+import {PDFLoader} from 'langchain/document_loaders/fs/pdf'
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import {PineconeStore} from 'langchain/vectorstores/pinecone'
+import { pinecone } from '../../../lib/pinecone';
  
 const f = createUploadthing();
  
@@ -28,8 +32,42 @@ export const ourFileRouter = {
       })
 
       try {
-        
+        const response = await fetch(`https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`)
+        const blob = await response.blob()
+        const loader = new PDFLoader(blob)
+        const pageLevelDocs = await loader.load()
+        const pagesAmt = pageLevelDocs.length
+
+        // vectorize and index entire document
+        const pineconeIndex = pinecone.Index("docly")
+        const embeddings = new OpenAIEmbeddings({
+          openAIApiKey: process.env.OPENAI_API_KEY
+        })
+
+        await PineconeStore.fromDocuments(pageLevelDocs, embeddings,
+          {
+          pineconeIndex, 
+          namespace: createdFile.id
+        })
+
+        await db.file.update({
+          data: {
+            uploadStatus: "SUCCESS",
+          },
+          where: {
+            id: createdFile.id
+          }
+        })
+
       } catch (error) {
+        await db.file.update({
+          data: {
+            uploadStatus: "FAILED",
+          },
+          where: {
+            id: createdFile.id
+          }
+        })
         
       }
 
